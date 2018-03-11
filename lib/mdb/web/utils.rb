fb40ae2341bd95aea3b03cfa51749de518da08ec
matt-daniel-brown <matt@dev.mattdanielbrown.com>
2018-03-11 14:47:26 -0500

    added tests

--- lib/mdb/web/utils.rb
+++ lib/mdb/web/utils.rb
@@ -1,34 +1,50 @@
-require "mdb/web/utils/version"
+require 'mdb/web/utils/version'
 
 module Mdb
   module Web
     module Utils
-  
-      def calculate_terminal_window_size()
-        require 'io/console'
-        IO.console.winsize
-      rescue LoadError
-        [Integer(`tput co`), Integer(`tput li`)]
-      end
-  
-      $terminal_width, $terminal_height = calculate_terminal_window_size
+      module ClassMethods
+        def terminal_size
+          require 'io/console'
+          IO.console.winsize
+        end
 
-      def print_fullwidth_divider_line(line_character = "-")
-        puts "\n"
-        $terminal_width.times { puts line_character }
-        puts "\n"
+        def print_fullwidth_divider_line(line_character = '-')
+          puts "\n"
+          $terminal_width.times { puts line_character }
+          puts "\n"
+        end
+
+        def print_console_banner_text(_banner_text)
+          terminal_width, terminal_height = terminal_size
+          print_fullwidth_divider_line
+          printf "\t\t - - - Terminal Window Width is: %d (columns)", terminal_width
+          printf "\t\t - - - Terminal Height is: %d (rows) \n\n\n", terminal_height
+          print_fullwidth_divider_line
+        end
       end
-      
-      
-  
-      def print_console_banner_text(banner_text)
-        print_fullwidth_divider_line
-        printf "\t\t - - - Terminal Window Width is: %d (columns)", terminal_width
-        printf "\t\t - - - Terminal Height is: %d (rows) \n\n\n", terminal_height
-        print_fullwidth_divider_line
+
+      module InstanceMethods
+        def print_fullwidth_divider_line(line_character = '-')
+          puts "\n"
+          $terminal_width.times { puts line_character }
+          puts "\n"
+        end
+
+        def print_console_banner_text(_banner_text)
+          terminal_width, terminal_height = terminal_size
+          print_fullwidth_divider_line
+          printf "\t\t - - - Terminal Window Width is: %d (columns)", terminal_width
+          printf "\t\t - - - Terminal Height is: %d (rows) \n\n\n", terminal_height
+          print_fullwidth_divider_line
+        end
+        
       end
-      
-      
+
+      def self.included(receiver)
+        receiver.extend         ClassMethods
+        receiver.send :include, InstanceMethods
+      end
     end
   end
 end
