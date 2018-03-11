964f62188766558c160cf7ba83c287d2bb0f7770
matt-daniel-brown <matt@dev.mattdanielbrown.com>
2018-03-11 11:22:07 -0500

    Initial Commit

---
+++ lib/mdb/web/utils.rb
@@ -0,0 +1,9 @@
+require "mdb/web/utils/version"
+
+module Mdb
+  module Web
+    module Utils
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/mdb/web/utils_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Mdb::Web::Utils do
+  it "has a version number" do
+    expect(Mdb::Web::Utils::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
